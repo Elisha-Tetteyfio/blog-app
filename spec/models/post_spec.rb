f1ec22026387dd376ed290182c7a5fafb2c767db
Elisha Tetteyfio <93135004+Elisha-Tetteyfio@users.noreply.github.com>
2022-09-30 19:55:05 +0000

    Make changes

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Post, type: :model do
   first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.',
                            posts_counter: 0)
   subject do
-    Post.create(id: 2, user: first_user, title: 'Hello', text: 'This is my first post', comments_counter: 0,
+    Post.create(id: rand(1000), user: first_user, title: 'Hello', text: 'This is my first post', comments_counter: 0,
                 likes_counter: 0)
   end
 
