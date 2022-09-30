9beed97cbde49474af3ca5d8802f07474fe5d74e
Elisha Tetteyfio <93135004+Elisha-Tetteyfio@users.noreply.github.com>
2022-09-30 18:24:43 +0000

    Add posts validation and tests

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,33 @@
+require './spec/rails_helper'
+
+RSpec.describe Post, type: :model do
+  #tests go here
+  first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+  subject { Post.create(user: first_user, title: 'Hello', text: 'This is my first post')}
+  before {subject.save}
+
+  it 'title must not be blank' do
+    subject.title = nil
+    expect(subject).to_not be_valid
+  end
+  it 'title must not exceed 250 characters' do
+    subject.title = "A"*255
+    expect(subject).to_not be_valid
+  end
+  it 'should increase user posts_counter' do
+    first_user.posts_counter = 3
+    expect(subject.update_post_count).to eql(4)
+  end
+  it 'comments_counter should be numeric' do
+    subject.comments_counter = "One"
+    expect(subject).to_not be_valid
+  end
+  it 'likes_counter should be numeric' do
+    subject.likes_counter = "One"
+    expect(subject).to_not be_valid
+  end
+  it 'recent comments should initially be 0' do
+    expect(subject.recent_comments.length).to eql(0)
+  end
+end
+
