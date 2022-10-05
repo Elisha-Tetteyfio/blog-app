68e413e62680733f18266e5f3ef73bd3f81a8147
Elisha Tetteyfio <93135004+Elisha-Tetteyfio@users.noreply.github.com>
2022-10-05 12:26:56 +0000

    Add tests for `show` method

--- spec/requests/posts_controller_spec.rb
+++ spec/requests/posts_controller_spec.rb
@@ -1,20 +1,41 @@
 require './spec/rails_helper'
 
 describe PostsController, type: :request do
-  before :each do
-    @link = get '/users/:id/posts'
-    get '/users/:id/posts'
+  describe "'/index' page" do
+    before :each do
+      @link = get '/users/:id/posts'
+      get '/users/:id/posts'
+    end
+
+    it 'Response value is 200' do
+      expect(@link).to eql(200)
+    end
+
+    it 'Renders a correct template' do
+      expect(response).to render_template('posts/index')
+    end
+
+    it 'Has the correct placeholder text' do
+      expect(response.body).to include('Blog App')
+    end
   end
 
-  it 'Response value is 200' do
-    expect(@link).to eql(200)
-  end
+  describe "'/show' page" do
+    before :each do
+      @link = get '/users/:id/posts/show'
+      get '/users/:id/posts/show'
+    end
 
-  it 'Renders a correct template' do
-    expect(response).to render_template('posts/index')
-  end
-
-  it 'Has the correct placeholder text' do
-    expect(response.body).to include('Blog App')
+    it 'Response value is 200' do
+      expect(@link).to eql(200)
+    end
+  
+    it 'Renders a correct template' do
+      expect(response).to render_template('posts/show')
+    end
+  
+    it 'Has the correct placeholder text' do
+      expect(response.body).to include('Blog app')
+    end
   end
 end
--- spec/requests/users_controller_spec.rb
+++ spec/requests/users_controller_spec.rb
@@ -1,20 +1,41 @@
 require './spec/rails_helper'
 
 describe UsersController, type: :request do
-  before :each do
-    @link = get users_path
-    get users_path
+  describe "'./index' page" do
+    before :each do
+      @link = get users_path
+      get users_path
+    end
+
+    it 'Response value is 200' do
+      expect(@link).to eql(200)
+    end
+
+    it 'Renders a correct template' do
+      expect(response).to render_template('users/index')
+    end
+
+    it 'Has the correct placeholder text' do
+      expect(response.body).to include('Blog App')
+    end
   end
 
-  it 'Response value is 200' do
-    expect(@link).to eql(200)
-  end
+  describe "'/show' page" do
+    before :each do
+      @link = get '/users/show'
+      get '/users/show'
+    end
 
-  it 'Renders a correct template' do
-    expect(response).to render_template('users/index')
-  end
-
-  it 'Has the correct placeholder text' do
-    expect(response.body).to include('Blog App')
+    it 'Response value is 200' do
+      expect(@link).to eql(200)
+    end
+  
+    it 'Renders a correct template' do
+      expect(response).to render_template('users/show')
+    end
+  
+    it 'Has the correct placeholder text' do
+      expect(response.body).to include('Blog app')
+    end
   end
 end
