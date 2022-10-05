514d18374a5482c5eb9cb3411714a967dcaa202c
Elisha Tetteyfio <93135004+Elisha-Tetteyfio@users.noreply.github.com>
2022-10-05 12:02:48 +0000

    Add posts_controller test

---
+++ spec/requests/posts_controller_spec.rb
@@ -0,0 +1,20 @@
+require './spec/rails_helper'
+
+describe PostsController, type: :request do
+  before :each do
+    @link = get '/users/:id/posts'
+    get '/users/:id/posts'
+  end
+
+  it 'Response value is 200' do
+    expect(@link).to eql(200)
+  end
+
+  it 'Renders a correct template' do
+    expect(response).to render_template('posts/index')
+  end
+
+  it 'Has the correct placeholder text' do
+    expect(response.body).to include("Blog App")
+  end
+end
