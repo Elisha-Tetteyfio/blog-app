18ae5d63e979ab33af63ef9c8c36227087403603
Elisha Tetteyfio <93135004+Elisha-Tetteyfio@users.noreply.github.com>
2022-10-07 14:02:04 +0000

    Add final page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,8 +2,11 @@ class PostsController < ApplicationController
   def index
     @posts = Post.where(user_id: params[:user_id].to_i)
     @user = User.find_by(id: params[:user_id].to_i)
-    @comments = Comment.where(post_id: params[:id].to_i)
   end
 
-  def show; end
+  def show
+    @post = Post.find_by(id: params[:id].to_i)
+    @user = User.find_by(id: params[:user_id].to_i)
+    @comments = Comment.where(post_id: params[:id].to_i)
+  end
 end
