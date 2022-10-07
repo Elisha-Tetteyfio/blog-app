e6d2364ca527a964df4672c708d3311c4b57eaa5
Elisha Tetteyfio <93135004+Elisha-Tetteyfio@users.noreply.github.com>
2022-10-07 13:46:44 +0000

    Show all posts for a user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,8 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @posts = Post.where(user_id: params[:user_id].to_i)
+    @user = User.find_by(id: params[:user_id].to_i)
+    @comments = Comment.where(post_id: params[:id].to_i)
   end
 
   def show; end
